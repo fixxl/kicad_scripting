342cfe501c41fcd823a51ffb1695970ae62133ae
Felix Pflaum <felix.pflaum@fau.de>
2018-04-13 15:06:12 +0200

    Added init-file which loads all the plugins

---
+++ __init__.py
@@ -0,0 +1,26 @@
+from font import *
+font().register()
+
+from board_outline import *
+board_outline().register()
+
+from Add_PCB_milling import *
+Add_PCB_milling().register()
+
+from Create_Rectangular_Polygons import *
+Create_Rectangular_Polygons().register()
+
+from Set_Pad_Clearance import *
+Set_Pad_Clearance().register()
+
+from Print_Board_Dimensions import *
+Print_Board_Dimensions().register()
+
+from Set_Value_Layer import *
+Set_Value_Layer().register()
+
+from Create_Output import *
+Create_Output().register()
+
+from Elecrow_Design_Rules import *
+Elecrow_Design_Rules().register()
